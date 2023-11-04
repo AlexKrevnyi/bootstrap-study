779fa0380d9649f9b0a8720e53f63db3c1d2f085
alex <sania9991@yahoo.com>
2023-11-03 17:42:05 -0700

    Other Components

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ if (toastTrigger) {
   toastTrigger.addEventListener('click', () => {
     toastBootstrap.show()
   })
-}
+};
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
